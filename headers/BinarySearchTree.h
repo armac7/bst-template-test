d1257c09075b3108522d4668e3d76df0537a70b1
armac7 <w.mcd0320@icloud.com>
2024-01-25 23:02:40 -0600

    Added Remove

--- headers/BinarySearchTree.h
+++ headers/BinarySearchTree.h
@@ -7,6 +7,9 @@ using namespace std;
 template <typename TYPE>
 class BinarySearchTree;
 
+//*************************************************
+// Node Declaration of Definition
+//*************************************************
 template <typename TYPE>
 class Node
 {
@@ -16,39 +19,82 @@ class Node
     Node() : left(nullptr), right(nullptr){};
     Node(TYPE key) : key(key), left(nullptr), right(nullptr){};
     Node(TYPE key, Node *l, Node *r) : key(key), left(nullptr), right(nullptr){};
+    Node<TYPE> *operator=(Node<TYPE> *node)
+    {
+        key = node->key;
+        left = node->left;
+        right = node->right;
+    };
     friend class BinarySearchTree<TYPE>;
 };
 
+//*************************************************
+// Binary Search Tree Declaration and Definition
+//*************************************************
 template <typename TYPE>
 class BinarySearchTree
 {
 public:
     BinarySearchTree() : root(nullptr){};
     BinarySearchTree(TYPE key) { Insert(key); };
-    // ~BinarySearchTree();
+    ~BinarySearchTree() { DeleteTree(); };
+    void DeleteTree()
+    {
+        DeleteTree(root);
+        root = nullptr;
+    }
     bool Insert(TYPE key) { return Insert(key, root); };
+    bool Remove(TYPE key) { return Remove(key, root); };
     Node<TYPE> *Search(TYPE key) const { Search(key, root); };
     void PrintInOrder() const
     {
-        cout << "< ";
-        PrintInOrder(root);
-        cout << ">";
+        if (root == nullptr)
+            Empty();
+        else
+        {
+            cout << "< ";
+            PrintInOrder(root);
+            cout << ">";
+        }
     };
     void PrintPreOrder() const
     {
-        cout << "< ";
-        PrintPreOrder(root);
-        cout << ">";
+        if (root == nullptr)
+            Empty();
+        else
+        {
+            cout << "< ";
+            PrintPreOrder(root);
+            cout << ">";
+        }
     };
     void PrintPostOrder() const
     {
-        cout << "< ";
-        PrintPostOrder(root);
-        cout << ">";
+        if (root == nullptr)
+            Empty();
+        else
+        {
+            cout << "< ";
+            PrintPostOrder(root);
+            cout << ">";
+        }
     };
 
 private:
     Node<TYPE> *root;
+    //*************************************************
+    // Helper Functions
+    //*************************************************
+    void DeleteTree(Node<TYPE> *&subroot) // Recursive delete function
+    {
+        if (subroot != nullptr)
+        {
+            // cout << subroot->key << " "; // debug
+            DeleteTree(subroot->left);
+            DeleteTree(subroot->right);
+            delete subroot;
+        }
+    }
     bool Insert(TYPE key, Node<TYPE> *&subroot)
     {
         bool success = true;
@@ -64,6 +110,49 @@ private:
             cout << "NO DUPLICATES ALLOWED!" << endl;
         }
         return success;
+    }
+    bool Remove(TYPE key, Node<TYPE> *&subroot)
+    {
+        bool succeed = true;
+        if (subroot == nullptr) // if key doesnt exist in bst or bst doesn't exist
+            succeed = false;
+        else
+        {
+            if (key < subroot->key) // continue searching to the left
+                Remove(key, subroot->left);
+            else if (key > subroot->key) // continue searching to the right
+                Remove(key, subroot->right);
+            else
+            {
+                if (subroot->left == nullptr) // if no left, shift right up to current subroot.
+                    subroot = subroot->right;
+                else if (subroot->right == nullptr) // if no right, shift left up to current subroot.
+                    subroot = subroot->left;
+                else // else if both subroots exist, find predecessor on the right, move up, and delete leaf
+                {
+                    Node<TYPE> *temp = findMin(subroot->right);
+                    subroot->key = temp->key;
+                    Remove(temp->key, subroot->right); // recursively delete leaf
+                }
+            }
+        }
+        return succeed;
+    }
+    Node<TYPE> *findMin(Node<TYPE> *subroot)
+    {
+        if (subroot->left == nullptr)
+            return subroot;
+        else
+            findMin(subroot->left);
+        return subroot;
+    }
+    Node<TYPE> *findMax(Node<TYPE> *subroot)
+    {
+        if (subroot->right == nullptr)
+            return subroot;
+        else
+            findMin(subroot->right);
+        return subroot;
     }
     Node<TYPE> *Search(TYPE key, Node<TYPE> *subroot) const
     {
@@ -105,6 +194,7 @@ private:
             cout << subroot->key << " ";
         }
     };
+    void Empty() const { cout << "Tree is empty." << endl; }
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,39 +2,105 @@
 #include "../headers/BinarySearchTree.h"
 using namespace std;
 
+void PrintMenu();
+int GetInput();
+void ExecuteOption(int input, BinarySearchTree<int> &tree);
+
 int main()
 {
     BinarySearchTree<int> tree;
-    tree.Insert(7);
-    tree.PrintInOrder();
-    cout << endl;
-    tree.Insert(4);
-    tree.PrintInOrder();
-    cout << endl;
-    tree.Insert(10);
-    tree.PrintInOrder();
-    cout << endl;
-    tree.Insert(2);
-    tree.PrintInOrder();
-    cout << endl;
-    tree.Insert(3);
-    tree.PrintInOrder();
-    cout << endl;
-    tree.Insert(6);
-    tree.PrintInOrder();
-    cout << endl;
-    tree.Insert(12);
-    tree.PrintInOrder();
-    cout << endl;
-    tree.Insert(13);
-    tree.PrintInOrder();
-    cout << endl;
-    tree.Insert(20);
-    tree.PrintInOrder();
-    cout << endl;
-    tree.PrintPreOrder();
-    cout << endl;
-    tree.PrintPostOrder();
-    cout << endl;
+    int input = 0;
+    while (input != 6)
+    {
+        PrintMenu();
+        input = GetInput();
+        if (input != 6)
+            ExecuteOption(input, tree);
+    }
+
+    // Test Calls
+    // tree.Insert(7);
+    // tree.PrintInOrder();
+    // cout << endl;
+    // tree.Insert(4);
+    // tree.PrintInOrder();
+    // cout << endl;
+    // tree.Insert(10);
+    // tree.PrintInOrder();
+    // cout << endl;
+    // tree.Insert(2);
+    // tree.PrintInOrder();
+    // cout << endl;
+    // tree.Insert(3);
+    // tree.PrintInOrder();
+    // cout << endl;
+    // tree.Insert(6);
+    // tree.PrintInOrder();
+    // cout << endl;
+    // tree.Insert(12);
+    // tree.PrintInOrder();
+    // cout << endl;
+    // tree.Insert(13);
+    // tree.PrintInOrder();
+    // cout << endl;
+    // tree.Insert(20);
+    // tree.PrintInOrder();
+    // cout << endl;
+    // tree.PrintPreOrder();
+    // cout << endl;
+    // tree.PrintPostOrder();
+    // cout << endl;
+    // tree.DeleteTree();
+    // tree.PrintInOrder();
     return 0;
+}
+
+void PrintMenu()
+{
+    cout << "***************************************" << endl;
+    cout << "1. Insert Into Tree" << endl;
+    cout << "2. Print In Order" << endl;
+    cout << "3. Print Preorder" << endl;
+    cout << "4. Print Postorder" << endl;
+    cout << "5. Delete Node" << endl;
+    cout << "6. Delete Tree" << endl;
+    cout << "7. Quit" << endl;
+    cout << "***************************************" << endl;
+}
+int GetInput()
+{
+    int input = 0;
+    while (input < 1 || input > 7)
+    {
+        cout << "Enter choice (1-7): ";
+        cin >> input;
+    }
+    return input;
+};
+void ExecuteOption(int input, BinarySearchTree<int> &tree)
+{
+    if (input == 1)
+    {
+        int data = 0;
+        cout << "Enter the number you want to put into the tree: ";
+        cin >> data;
+        tree.Insert(data);
+    }
+    else if (input == 2)
+        tree.PrintInOrder();
+    else if (input == 3)
+        tree.PrintPreOrder();
+    else if (input == 4)
+        tree.PrintPostOrder();
+    else if (input == 5)
+    {
+        int data = 0;
+        cout << "Enter the number you want to remove: ";
+        cin >> data;
+        tree.Remove(data);
+    }
+    else if (input == 6)
+        tree.DeleteTree();
+    cout << endl;
+    cout << endl;
 }
